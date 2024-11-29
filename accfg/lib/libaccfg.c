8ddaae408deefde1175bbf7f694298fefa56eacb
Yi Sun <yi.sun@intel.com>
2024-11-29 18:29:36 +0800

    accfg: Add Durable Write flag Support
    For DSA3.0 spec, add durable_wr per devices.
    
    Signed-off-by: Yi Sun <yi.sun@intel.com>

--- accfg/lib/libaccfg.c
+++ accfg/lib/libaccfg.c
@@ -584,6 +584,8 @@ static void *add_device(void *parent, int id, const char *ctl_base,
 	device->event_log_size = accfg_get_param_long(ctx, dfd, "event_log_size");
 	device->cdev_major = accfg_get_param_long(ctx, dfd, "cdev_major");
 	device->version = accfg_get_param_unsigned_llong(ctx, dfd, "version");
+	device->durable_wr_opt_out = accfg_get_param_long(ctx, dfd,
+			"durable_wr_opt_out");
 	device->device_path = realpath(ctl_base, NULL);
 	close(dfd);
 	if (!device->device_path) {
@@ -1256,6 +1258,12 @@ ACCFG_EXPORT unsigned int accfg_device_get_version(
 	return device->version;
 }
 
+ACCFG_EXPORT int accfg_device_get_durable_wr_opt_out(
+		struct accfg_device *device)
+{
+	return device->durable_wr_opt_out;
+}
+
 ACCFG_EXPORT int accfg_device_get_clients(struct accfg_device *device)
 {
 	struct accfg_ctx *ctx;
@@ -1285,6 +1293,53 @@ ACCFG_EXPORT int accfg_device_get_clients(struct accfg_device *device)
 	}
 
 	return atoi(buf);
+}
+
+ACCFG_EXPORT int accfg_device_set_durable_wr_opt_out(struct accfg_device *dev, int val)
+{
+	struct accfg_ctx *ctx;
+	char *path;
+	char buf[SYSFS_ATTR_SIZE];
+
+	if (!dev)
+		return -EINVAL;
+
+	path = dev->device_buf;
+	ctx = accfg_device_get_ctx(dev);
+
+	if (sprintf(path, "%s/durable_wr_opt_out", dev->device_path) >=
+			(int)dev->buf_len) {
+		err(ctx, "%s; buf len exceeded.\n",
+				accfg_device_get_devname(dev));
+		return -errno;
+	}
+
+	if (access(path, F_OK)) {
+		if (sprintf(path, "%s/%s", dev->device_path,
+					deprecated_attr("durable_wr_opt_out")) >=
+					(int)dev->buf_len) {
+			err(ctx, "%s; buf len exceeded.\n",
+					accfg_device_get_devname(dev));
+			return -errno;
+		}
+	}
+
+	if (sprintf(buf, "%d", val) < 0) {
+		err(ctx, "%s: sprintf to buf failed: %s\n",
+				accfg_device_get_devname(dev), strerror(errno));
+		return -errno;
+	}
+
+	if (sysfs_write_attr(ctx, path, buf) < 0) {
+		err(ctx, "%s: write failed: %s\n",
+				accfg_device_get_devname(dev), strerror(errno));
+		save_last_error(dev, NULL, NULL, NULL);
+		return -errno;
+	}
+
+	dev->durable_wr_opt_out = val;
+
+	return 0;
 }
 
 ACCFG_EXPORT int accfg_device_set_read_buffer_limit(struct accfg_device *dev, int val)
--- util/json.c
+++ util/json.c
@@ -188,6 +188,14 @@ struct json_object *util_device_to_json(struct accfg_device *device,
 		json_object_object_add(jdevice, "event_log_size", jobj);
 	}
 
+	int_val = accfg_device_get_durable_wr_opt_out(device);
+	if (int_val >= 0) {
+		jobj = json_object_new_int(int_val);
+		if (!jobj)
+			goto err;
+		json_object_object_add(jdevice, "durable_wr_opt_out", jobj);
+	}
+
 	if (flags & UTIL_JSON_SAVE) {
 		free(error);
 		return jdevice;
